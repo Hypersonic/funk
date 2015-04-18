9054992504f3dd7f3271d69b6db165a72d12683e
Josh Hofing <joshhofing@gmail.com>
2015-04-17 21:27:19 -0400

    Use pre and post conditions on all fib impls

--- demos.py
+++ demos.py
@@ -9,12 +9,16 @@ def fib_match(n):
     '''
     return fib_match(n-1) + fib_match(n-2)
 
+@precondition(lambda x: x >= 0)
+@postcondition(lambda x: x > 0)
 @match_pred((lambda n: n <= 1,), 1)
 def fib_pred(n):
     ''' Fibonacci using predicate-based pattern matching
     '''
     return fib_pred(n-1) + fib_pred(n-2)
 
+@precondition(lambda x: x >= 0)
+@postcondition(lambda x: x > 0)
 @memoize
 def fib_memo(n):
     ''' Memoized fibnacci
@@ -22,6 +26,8 @@ def fib_memo(n):
     if n <= 1: return 1
     return fib_memo(n-1) + fib_memo(n-2)
 
+@precondition(lambda x: x >= 0)
+@postcondition(lambda x: x > 0)
 @lookup_table([0], [1], [10], [20], [30])
 def fib_lookup(n):
     ''' Fibonacci with a lookup table for certain values
